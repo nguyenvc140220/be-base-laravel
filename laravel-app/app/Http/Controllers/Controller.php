e250e74ecd13ae2220961df748e038d75f0a6dfc
Minh Luu <minh.luu@pionero.io>
2024-11-11 05:51:04 +0000

    Revert "Merge branch 'feature/setup-swagger' into 'main'"
    This reverts merge request !4

--- laravel-app/app/Http/Controllers/Controller.php
+++ laravel-app/app/Http/Controllers/Controller.php
@@ -8,17 +8,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info( title="Base Laravel API", version="1.0" )
- * @OA\Server( url="http://localhost:8001/api/v1/" )
- * @OA\SecurityScheme(
- *     securityScheme="bearerAuth",
- *     type="http",
- *     scheme="bearer",
- *     bearerFormat="JWT"
- * )
- * @OA\PathItem ( path="/api/v1" )
- */
 class Controller extends BaseController {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponseTrait;
 }
--- laravel-app/app/Http/Resources/Auth/AuthResource.php
+++ laravel-app/app/Http/Resources/Auth/AuthResource.php
@@ -4,17 +4,6 @@ namespace App\Http\Resources\Auth;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/**
- * @OA\Schema(
- *     schema="AuthResource",
- *     type="object",
- *     @OA\Property(
- *     property="accept_token",
- *     type="string",
- *     example="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ZjIwZjQwZjQwZjQwZjQwZjQwZjQwZjQiLCJpYXQiOjE1OTYwNzQwNzYsImV4cCI6MTU5NjA3NzY3Nn0.7"
- *    )
- * )
- */
 class AuthResource extends JsonResource {
     public function toArray($request): array {
         return ['accept_token' => $this['token']];
